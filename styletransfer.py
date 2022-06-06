fe0258c602b2af0580eade8199d8677561aa10bb
supplyandcommand <42962106+supplyandcommand@users.noreply.github.com>
2022-06-06 19:19:57 +0200

    minimal working example of style transfer and text2img

---
+++ styletransfer.py
@@ -0,0 +1,101 @@
+# %%
+import os
+
+import streamlit as st
+import matplotlib.pyplot as plt
+
+import numpy as np
+import tensorflow as tf
+import tensorflow_hub as hub
+
+from util import crop_center, load_img_url, load_img_path, show_n, transform_img, tensor_to_image
+
+
+# # Only use the below code if you have low resources.
+# os.environ['CUDA_VISIBLE_DEVICES'] = ""
+# os.environ['TFHUB_MODEL_LOAD_FORMAT'] = 'COMPRESSED'
+
+# # For supressing warnings
+# os.environ['TF_CPP_MIN_LOG_LEVEL'] = '2'
+
+# streamlit config
+st.set_page_config(
+    page_title="Fuckery",
+    page_icon="🎈",
+)
+
+# model config
+@st.cache
+def get_model():
+    hub_handle = 'https://tfhub.dev/google/magenta/arbitrary-image-stylization-v1-256/2'
+    hub_module = hub.load(hub_handle)
+    return hub_module
+
+model = get_model()
+
+
+# width
+def _max_width_():
+    max_width_str = f"max-width: 1400px;"
+    st.markdown(
+        f"""
+    <style>
+    .reportview-container .main .block-container{{
+        {max_width_str}
+    }}
+    </style>    
+    """,
+        unsafe_allow_html=True,
+    )
+
+_max_width_()
+
+st.title("Style transfer fun zone")
+st.header("")
+
+with st.expander("ℹ️ - About this app", expanded=True):
+
+    st.write(
+        """
+        Careful. I'm up to here guys.
+	    """
+    )
+    st.markdown("")
+
+# set up dividers
+col1, col2 = st.columns(2)
+
+with col1:
+    st.write('Content')
+    content_image_file = st.file_uploader(
+        "Upload main image", type=("png", "jpg"))
+    try:
+        content_image_file = content_image_file.read()
+        content_image = transform_img(content_image_file)
+    except:
+        pass
+
+with col2:
+    st.write('Style')
+    style_image_file = st.file_uploader(
+        "Upload style image", type=("png", "jpg"))
+    try:
+        style_image_file = style_image_file.read()
+        style_image = transform_img(style_image_file)
+        style_image = tf.nn.avg_pool(style_image, ksize=[3,3], strides=[1,1], padding='SAME')
+    except:
+        pass
+
+
+# %%
+# stylize
+predict = st.button('Start Neural Style Transfer...')
+
+if predict:
+    outputs = model(tf.constant(content_image), tf.constant(style_image))
+    stylized_image = outputs[0]
+    # show_n([content_image, style_image, stylized_image], titles=['Original content image', 'Style image', 'Stylized image'])
+    final_img = tensor_to_image(stylized_image)
+    st.image(final_img)
+
+# %%
---
+++ util.py
@@ -0,0 +1,75 @@
+# @title Define image loading and visualization functions  { display-mode: "form" }
+import os
+import numpy as np
+import tensorflow as tf
+import matplotlib.pyplot as plt
+from matplotlib import gridspec
+from PIL import Image
+
+def crop_center(image):
+    """Returns a cropped square image."""
+    shape = image.shape
+    new_shape = min(shape[1], shape[2])
+    offset_y = max(shape[1] - shape[2], 0) // 2
+    offset_x = max(shape[2] - shape[1], 0) // 2
+    image = tf.image.crop_to_bounding_box(
+        image, offset_y, offset_x, new_shape, new_shape)
+    return image
+
+def load_img_url(image_url, image_size=(256, 256), preserve_aspect_ratio=True):
+    """Loads and preprocesses images."""
+    # Cache image file locally.
+    image_path = tf.keras.utils.get_file(os.path.basename(image_url)[-128:], image_url)
+    # Load and convert to float32 numpy array, add batch dimension, and normalize to range [0, 1].
+    img = tf.io.decode_image(
+        tf.io.read_file(image_path),
+        channels=3, dtype=tf.float32)[tf.newaxis, ...]
+
+    img = crop_center(img)
+    img = tf.image.resize(img, image_size, preserve_aspect_ratio=True)
+    return img
+
+def load_img_path(image_path, image_size=(256, 256), preserve_aspect_ratio=True):
+    img = tf.io.decode_image(
+        tf.io.read_file(image_path),
+        channels=3, dtype=tf.float32)[tf.newaxis, ...]
+    img = crop_center(img)
+    img = tf.image.resize(img, image_size, preserve_aspect_ratio=True)
+    return img
+
+def show_n(images, titles=('',)):
+    n = len(images)
+    image_sizes = [image.shape[1] for image in images]
+    w = (image_sizes[0] * 6) // 320
+    plt.figure(figsize=(w * n, w))
+    gs = gridspec.GridSpec(1, n, width_ratios=image_sizes)
+    for i in range(n):
+        plt.subplot(gs[i])
+        plt.imshow(images[i][0], aspect='equal')
+        plt.axis('off')
+        plt.title(titles[i] if len(titles) > i else '')
+        plt.show()
+
+def transform_img(img):
+    max_dim = 512
+    img = tf.image.decode_image(img)
+    img = tf.image.convert_image_dtype(img, tf.float32)
+
+    shape = tf.cast(tf.shape(img)[:-1], tf.float32)
+    long_dim = max(shape)
+    scale = max_dim / long_dim
+
+    new_shape = tf.cast(shape * scale, tf.int32)
+
+    img = tf.image.resize(img, new_shape)
+    img = img[tf.newaxis, :]
+    return img
+
+
+def tensor_to_image(tensor):
+    tensor = tensor * 255
+    tensor = np.array(tensor, np.uint8)
+    if np.ndim(tensor) > 3:
+        assert tensor.shape[0] == 1
+        tensor = tensor[0]
+    return Image.fromarray(tensor)
